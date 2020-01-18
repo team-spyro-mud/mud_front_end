c5b4dc1f43379f5d9e025288786fcb61d18eb319
Reyaad Rafeek <reyaad510@gmail.com>
2020-01-17 20:06:09 -0500

    Have rooms displaying but not in right positions yet

--- frontend/src/components/DisplayMap.jsx
+++ frontend/src/components/DisplayMap.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 
+const worldBox = {
+  border: "1px solid red",
+  display: "flex",
+  width: "90%",
+  height: "75vh"
+};
+
+const mapDisplayAsBox = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100px",
+  height: "100px",
+  backgroundColor: "black",
+  color: "green",
+  margin: "10px"
+};
+
 class DisplayMap extends Component {
   constructor(props) {
     super(props);
   }
-
+  // <h1>{room.fields.title}</h1>
   render() {
     return (
-      <div>
+      <div style={worldBox}>
         {this.props.rooms.map(room => {
-          return <div>Hi</div>;
+          return <div style={mapDisplayAsBox}>{room.fields.title}</div>;
         })}
       </div>
     );
